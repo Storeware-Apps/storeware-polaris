d41753d739fb409a3dd6ced24e7fb7885d018197
mdhossain07 <mrabir8097@gmail.com>
2025-07-20 11:01:37 +0600

    chore: add global CSS import to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import "./styles/globals.css";
+
 // Export main Button component with both Polaris API and shadcn/ui functionality
 export { Button, buttonVariants } from "./components/Button/button.js";
 
